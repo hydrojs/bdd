9bebc6631b971e2aaa142b24a645a5f3845014cd
Veselin Todorov <veselin@veselin.bg>
2013-12-10 14:02:26 +0200

    Do not bother with plugin details for now

--- index.js
+++ index.js
@@ -8,16 +8,4 @@
 module.exports = function(hydro) {
   hydro.set('proxies', 'describe', 'addSuite');
   hydro.set('proxies', 'it', 'addTest');
-}
-
-/**
- * Plugin title.
- */
-
-module.exports.title = 'hydro-bdd';
-
-/**
- * Plugin description.
- */
-
-module.exports.desc = 'BDD interface (describe, it)';
+};
