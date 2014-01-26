a9fc7bd61d542582225e3b5f32afe173c2d8e33e
Veselin Todorov <veselin@veselin.bg>
2014-01-26 20:38:17 +0200

    Release 0.2.0

--- hydro-bdd.js
+++ hydro-bdd.js
@@ -211,8 +211,15 @@ require.register("hydro-bdd/index.js", function(exports, require, module){
 module.exports = function(hydro) {
   hydro.set('proxies', 'describe', 'addSuite');
   hydro.set('proxies', 'context', 'addSuite');
-  hydro.set('proxies', 'when', 'addSuite');
   hydro.set('proxies', 'it', 'addTest');
+
+  hydro.set('proxies', 'before', 'before');
+  hydro.set('proxies', 'beforeAll', 'beforeAll');
+  hydro.set('proxies', 'beforeNext', 'beforeNext');
+
+  hydro.set('proxies', 'after', 'after');
+  hydro.set('proxies', 'afterAll', 'afterAll');
+  hydro.set('proxies', 'afterNext', 'afterNext');
 };
 
 });
